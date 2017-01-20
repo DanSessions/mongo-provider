867b46db24c7658dcff509bdd939ae4ce94bb219
Dan Sessions <dansessions@gmail.com>
2017-01-20 15:09:46 +0000

    Fixed dependancy version

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "VaporMongo",
     dependencies: [
     	.Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
-		.Package(url: "https://github.com/DanSessions/mongo-driver.git", majorVersion: 1)
+		.Package(url: "https://github.com/DanSessions/mongo-driver.git", Version(1, 0, 10))
     ]
 )
